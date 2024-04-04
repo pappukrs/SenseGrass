004a605c890b6c1c240a44723c2877ad55e4a0c9
pappukrs <digitalpappu101@gmail.com>
2024-04-04 22:11:42 +0530

    added top-vaigation tab bottom-navigation-tab add payment method etc

---
+++ munafaClient/screens/Home/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <View>
+        <Text>
+            ExpensesScreen
+        </Text>
+    </View>
+  )
+}
+
+export default HomeScreen
---
+++ munafaClient/screens/Payments/CreditCard.jsx
@@ -0,0 +1,57 @@
+// CreditCardScreen.js
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+const CreditCardScreen = () => {
+  const [cardNumber, setCardNumber] = useState('');
+  const [expiryDate, setExpiryDate] = useState('');
+  const [cvv, setCvv] = useState('');
+
+  const handleSave = () => {
+    // Logic to save credit card details
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Card Number"
+        value={cardNumber}
+        onChangeText={setCardNumber}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Expiry Date"
+        value={expiryDate}
+        onChangeText={setExpiryDate}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="CVV"
+        value={cvv}
+        onChangeText={setCvv}
+      />
+      <Button title="Save" onPress={handleSave} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+});
+
+export default CreditCardScreen;
